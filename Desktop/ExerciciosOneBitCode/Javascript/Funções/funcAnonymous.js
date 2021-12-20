deb68ac5063944b05b07a6fadd530b87396eb521
Guilherme de Oliveira Barros <47544503+FIXER3600@users.noreply.github.com>
2021-12-20 17:51:30 -0300

    Delete Desktop/ExerciciosOneBitCode directory

--- Desktop/ExerciciosOneBitCode/Javascript/Funções/funcAnonymous.js
+++
@@ -1,6 +0,0 @@
-let doubleSpeed = function (velocity) {
-	return velocity * 2;
-}
-let newVelocity = doubleSpeed(50)
-
-document.write('O dobro da velocidade no argumento da função anônima '+ doubleSpeed+' é: '+newVelocity)
