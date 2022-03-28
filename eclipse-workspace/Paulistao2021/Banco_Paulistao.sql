dbec76b9f2f58c02e29a938c168a152bee3569d9
GabrielFernandez26 <79326423+GabrielFernandez26@users.noreply.github.com>
2022-03-27 21:58:24 -0300

    Update Banco_Paulistao.sql

--- eclipse-workspace/Paulistao2021/Banco_Paulistao.sql
+++ eclipse-workspace/Paulistao2021/Banco_Paulistao.sql
@@ -1,6 +1,6 @@
 create database db_paulistao
-use db_paulistao
 go
+use db_paulistao
 
 create table Times(
 	CodigoTime int not null,
@@ -10,11 +10,11 @@ create table Times(
 	primary key(CodigoTime)
 )
 create table Grupos(
-		Grupo varchar (2) not null,
-		CodigoTime int not null,
-		Jogos int not null,
-		foreign key (CodigoTime) references Times(CodigoTime)
+	Grupo varchar (2) not null,
+	CodigoTime int not null,
+	foreign key (CodigoTime) references Times(CodigoTime)
 )
+
 create table Jogos(
 	CodigoTimeA INT not null,
 	CodigoTimeB int not null,
@@ -22,3 +22,29 @@ create table Jogos(
 	GolsTimeB int,
 	Data date not null
 )
+
+INSERT INTO Times VALUES
+(1, 'Botafogo', 'Ribeirão Preto', 'Santa Cruz'),
+(2, 'Corinthians', 'São Paulo', 'Neo Química Arena'),
+(3, 'Ferroviária', 'Araraquara', 'Fonte Luminosa'),
+(4, 'Guarani', 'Campinas', 'Brinco de Ouro'),
+(5, 'Inter de Limeira', 'Limeira', 'Limeirão'),
+(6, 'Ituano', 'Itu', 'Novelli Júnior'),
+(7, 'Mirassol', 'Mirassol', 'José Maria de Campos Maia'),
+(8, 'Novorizontino', 'Novo Horizonte', 'Jorge Ismael de Biasi'),
+(9, 'Palmeiras', 'São Paulo', 'Allianz Parque'),
+(10, 'Ponte Preta', 'Campinas', 'Moisés Lucarelli'),
+(11, 'Red Bull Bragantino', 'Bragança Paulista', 'Nabi Abi Chedid'),
+(12, 'Santo André', 'Santo André', 'Bruno José Daniel'),
+(13, 'Santos', 'Santos', 'Vila Belmiro'),
+(14, 'São Bento', 'Sorocaba', 'Walter Ribeiro'),
+(15, 'São Caetano', 'São Caetano do Sul', 'Anacleto Campanella'),
+(16, 'São Paulo', 'São Paulo', 'Morumbi')
+
+INSERT INTO Grupos VALUES 
+('A', 2),
+('B', 9),
+('C', 13),
+('D', 16)
+
+
